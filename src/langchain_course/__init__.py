e9e0facb364aaab159244c31c9dd3aecdfbf7061
jayashreema <rangili.ma678@gmail.com>
2026-09-08 11:55:44 +0530

    environment setup

---
+++ src/langchain_course/__init__.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main() -> None:
+    print("Hello from langchain-course!")
+    # print(os.environ.get("OPENAI_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
